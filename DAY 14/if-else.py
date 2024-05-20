91813f8e04524d66f7d9dee97584f1d00f45ae54
Abhishek Shakya <abshakya63@GMAIL.COM>
2024-05-20 22:48:03 +0530

    Add files via upload

---
+++ DAY 14/if-else.py
@@ -0,0 +1,26 @@
+# In this code we are going to study if-else
+# if-else is a conditional statement that allows us to execute a block of code if a certain condition is true, or another block of code if
+# the condition is false. The syntax for an if-else statement is as follows:
+# if condition:
+#     # code to execute if condition is true
+# else:
+#     # code to execute if condition is false
+
+
+
+a = int(input("Enter your age:"))
+print("Your age is:",a)
+
+if(a >= 18):
+    print("You are eligible to vote") #This code is inside the if-block.
+    # This block of code executes only when the condition is true.
+
+
+else:
+    print("You are not eligible to vote") #his code is inside the else-block.
+    # This block of code executes only when the condition is false.
+
+
+# Now learn about elif condition
+
+
