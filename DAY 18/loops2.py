d658f807e2e59885b20afb079acd103897d14459
Abhishek Shakya <abshakya63@GMAIL.COM>
2024-05-25 21:23:52 +0530

    Add files via upload

---
+++ DAY 18/loops2.py
@@ -0,0 +1,21 @@
+# We are going to learn about while loop
+# while loop is used to repeat a block of code until a condition is met/true
+
+# Syntax: 
+# while (condition) :
+#     code to be executed
+
+# Example
+
+for i in range(3):
+    print(i)
+
+# The above loop print the number till 2
+# We can perform this by while loop
+
+print("\n")
+
+i = 0
+while(i < 3):
+    print(i)
+    i = i+1
