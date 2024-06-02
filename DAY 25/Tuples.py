ef8d0ed43df4a6b4bdab3fb80a9dd26430167d3c
Abhishek Shakya <abshakya63@GMAIL.COM>
2024-06-02 23:30:59 +0530

    Add files via upload

---
+++ DAY 25/Tuples.py
@@ -0,0 +1,19 @@
+# We are going to learn tuples
+# What is tuples?
+# Tuples are like lists but they are immutable. They are like a read-only list.
+# Tuples are like lists but they are immutable
+# Tuples are used to store multiple items in a single variable
+# Tuples are written with round brackets
+# Tuples are used to store multiple items in a single variable
+# Tuples items are seperated with the commas and enclosed within round brackets.
+
+
+# Examples
+
+tup = (1,2,3,4)
+print(tup)
+print(type(tup))
+
+# Creating a tuple with one item
+tup1 = (1,) #It consist commas after first element. It tells the python compilter that it is a tuple not a integer number.
+print(tup1)
