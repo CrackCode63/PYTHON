a42e766b7bac29fe232476f569b911c615489179
Abhishek Shakya <abshakya63@GMAIL.COM>
2024-06-06 23:51:05 +0530

    Add files via upload

---
+++ DAY 31/Sets.py
@@ -0,0 +1,10 @@
+# What is sets in python?
+# Sets are unordered collection of data items. They store multiple items in a single variable. Set items are separated by commas and enclosed within curly brackets {}. Sets are unchangeable, meaning you cannot change items of the set once created. Sets do not contain duplicate items.
+# Example:
+info = {"Carla", 19, False, 5.9}
+print(info)
+
+# Creating an empty set
+# Example:
+set = {}
+print(type(set))
