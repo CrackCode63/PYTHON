08b48f4d35d16f3cd4600d4605f9e084d03c81f1
Abhishek Shakya <abshakya63@GMAIL.COM>
2024-08-11 22:48:37 +0530

    Add files via upload

---
+++ DAY 53/main.py
@@ -0,0 +1,10 @@
+# what is lambda funtion?
+# In Python, a lambda function (also known as an anonymous function) is a small, single-line function that can be defined inline within a larger expression. It's a shorthand way to create a function without declaring a separate named function using the def keyword.
+
+# The syntax for a lambda function is:
+# lambda arguments: expression
+# where arguments is a comma-separated list of variables that will be passed to the function, and expression is the code that will be executed when the function is called.
+
+
+double = lambda x: x * 2
+print(double(5))  # Output: 10
