bd5c26cc243b136db6c9e737c4347443b61fb4a2
Abhishek Shakya <abshakya63@GMAIL.COM>
2024-08-13 23:51:53 +0530

    Add files via upload

---
+++ DAY 55/main.py
@@ -0,0 +1,15 @@
+# is and == in python
+# is is used to check if both variables are pointing to the same object in memory
+
+
+
+a = 3;
+b = "3";
+print(a is b) # checks for the location in the memory
+print(a != b) # False 
+
+
+a = 3;
+b = 3;
+print(a is b) # checks for the location in the memory
+print(a != b) # False 
